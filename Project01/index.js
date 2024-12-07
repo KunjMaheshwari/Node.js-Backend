cd778a0de70d674f11d6dcd6a14729cf3c5d7e48
Kunj Maheshwari <kunj.maheshwari2021@vitbhopal.ac.in>
2024-12-07 23:54:27 +0530

    Connecting Node.js with MongoDB

--- Project01/index.js
+++ Project01/index.js
@@ -1,12 +1,43 @@
 const express = require("express");
-
 const fs = require("fs");
 
+const mongoose = require("mongoose");
+
 // import the data from MOCK_DATA.json file 
-const users = require("./MOCK_DATA.json");
+// const users = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 8000;
+
+//Connection
+mongoose.connect('mongodb://127.0.0.1:27017/youtube-app-1')
+.then(() => console.log("MongoDB Connected."))
+.catch(err => console.log("Mongo Error", err));
+
+//Schema
+const userSchema = new mongoose.Schema({
+    first_name:{
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String, 
+        required: true
+    },
+    email:{
+        type: String, 
+        required: true,
+        unique: true,
+    },
+    jobTitle: {
+        type:String
+    },
+    gender:{
+        type:String
+    }
+}, {timestamps: true})
+
+const User = mongoose.model("user", userSchema);
 
 app.use(express.urlencoded({ extended: false })); // middleware and this is a built in middleware.
 //this middleware basically converts all the data that we are getting from frontend to Objects.
@@ -32,25 +63,28 @@ app.use((res, req, next) => {
     );
 });
 
-app.get("/users", (req, res) => {
+app.get("/users", async(req, res) => {
+    const allDbUsers = await User.find({});
     const html = `
     <ul>
-        ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+        ${allDbUsers.map((user) => `<li>${user.firstName} - ${user.email}</li>`).join("")}
     </ul>
     `;
     res.send(html);
 })
 
 //REST API 
-app.get("/api/users", (req, res) => {
-    res.setHeader("X-myName", "Kunj Maheshwari"); // added a Custom HTTP Header.
-    // always add X to a custom header.
-    return res.json(users);
+app.get("/api/users", async(req, res) => {
+    const allDbUsers = await User.find({});
+    // res.setHeader("X-myName", "Kunj Maheshwari"); // added a Custom HTTP Header.
+    // // always add X to a custom header.
+    return res.json(allDbUsers);
 })
 
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find((user) => user.id === id);
+app.get("/api/users/:id", async(req, res) => {
+    // const id = Number(req.params.id);
+    // const user = users.find((user) => user.id === id);
+    const user = await User.findById(req.params.id);
     if(!user) return res.status(404).json({ msg: "Invalid id."});
     return res.json(user);
 })
@@ -84,18 +118,29 @@ app.patch("/api/users/:id", (req, res) => {
     });
 });
 
-app.delete("/api/users/:id", (req, res) => {
+app.delete("/api/users/:id", async(req, res) => {
     const { id } = req.params;
 
     const userIndex = users.findIndex(user => user.id === parseInt(id));
-    if (userIndex === -1) return res.status(404).json({ status: "error", message: "User not found" });
+    if (userIndex === -1) {
+        return res.status(404).json({ status: "error", message: "User not found" });
+    }
 
-    const deletedUser = users.splice(userIndex, 1);
+    // const deletedUser = users.splice(userIndex, 1);
 
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
-        if (err) return res.status(500).json({ status: "error", message: "Failed to delete user" });
-        res.json({ status: "success", data: deletedUser[0] });
-    });
+    // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+    //     if (err) return res.status(500).json({ status: "error", message: "Failed to delete user" });
+    //     res.json({ status: "success", data: deletedUser[0] });
+    // });
+    const result = await User.create({
+        firstName: body.first_name,
+        lastName: body.last_name,
+        email: body.email,
+        gender: body.gender,
+        jobTitle: body.job_title
+    })
+
+    return res.status(201).json({msg: "Success"});
 });
 
 
